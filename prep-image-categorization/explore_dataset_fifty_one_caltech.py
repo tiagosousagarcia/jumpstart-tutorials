4bbb99f6438b47cc13450d4d8717d2db114982f3
tiagosousagarcia <tiagosousagarcia@gmail.com>
2022-05-16 09:09:59 +0100

    added data for image cat

--- prep-image-categorization/explore_dataset_fifty_one_caltech.py
+++ prep-image-categorization/explore_dataset_fifty_one_caltech.py
@@ -2,18 +2,13 @@ import fiftyone as fo
 import fiftyone.zoo as foz
 from fiftyone import ViewField as F
 
-dataset = foz.load_zoo_dataset("caltech101", max_samples=5000, shuffle=True)
+dataset = foz.load_zoo_dataset("caltech101", max_samples=1000, shuffle=True)
 
 view = dataset.filter_labels("ground_truth", (F("label") == "Motorbikes") | 
                                             (F("label") == "airplanes") |
                                             (F("label") == "Faces") |
-                                            (F("label") == "watch") |
-                                            (F("label") == "Leopards") |
-                                            (F("label") == "chandelier") |
                                             (F("label") == "butterfly") |
-                                            (F("label") == "sunflower") |
-                                            (F("label") == "kangaroo") |
-                                            (F("label") == "laptop") )
+                                            (F("label") == "sunflower"))
 
 session = fo.launch_app(view)
 
--- prep-image-categorization/export_dataset_caltech.py
+++ prep-image-categorization/export_dataset_caltech.py
@@ -10,7 +10,7 @@ import fiftyone as fo
 import fiftyone.zoo as foz
 from fiftyone import ViewField as F
 
-dataset = foz.load_zoo_dataset("caltech101", max_samples=10000, shuffle=True)
+dataset = foz.load_zoo_dataset("caltech101", max_samples=1000, shuffle=True)
 
 export_dir = "caltech101_dataset/"
 label_field = "ground_truth"  # for example
@@ -18,14 +18,20 @@ label_field = "ground_truth"  # for example
 view = dataset.filter_labels("ground_truth", (F("label") == "Motorbikes") | 
                                             (F("label") == "airplanes") |
                                             (F("label") == "Faces") |
-                                            (F("label") == "watch") |
-                                            (F("label") == "Leopards") |
-                                            (F("label") == "chandelier") |
                                             (F("label") == "butterfly") |
-                                            (F("label") == "sunflower") |
-                                            (F("label") == "kangaroo") |
-                                            (F("label") == "laptop") |
-                                            (F("label") == "BACKGROUND_Google"))
+                                            (F("label") == "sunflower"))
+
+# view = dataset.filter_labels("ground_truth", (F("label") == "Motorbikes") | 
+#                                             (F("label") == "airplanes") |
+#                                             (F("label") == "Faces") |
+#                                             (F("label") == "watch") |
+#                                             (F("label") == "Leopards") |
+#                                             (F("label") == "chandelier") |
+#                                             (F("label") == "butterfly") |
+#                                             (F("label") == "sunflower") |
+#                                             (F("label") == "kangaroo") |
+#                                             (F("label") == "laptop") |
+#                                             (F("label") == "BACKGROUND_Google"))
 
 # Export the dataset
 view.export(
